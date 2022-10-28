e60bed974472565600716a26ad311931919aa57e
loladmebrah <shernandezh@unal.edu.co>
2022-10-28 18:39:46 -0500

    updated indexer, missing reward list per action

--- indexer/index.js
+++ indexer/index.js
@@ -1,10 +1,9 @@
-//const collectionStoreABI = require('./contracts-abi/collectionStore').abi;
+const creatorABI = require('./contracts-abi/creator').abi;
 require('dotenv').config();
 
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.RPC_NODE_ADDRESS));
 const contracts = JSON.parse(process.env.CONTRACTS_OBJECT);
-console.log("!", contracts.length)
 
 async function subscribeToEvents(contractAddress, topics, startingBlock, tag){
     web3.eth.subscribe('logs',{
@@ -14,43 +13,73 @@ async function subscribeToEvents(contractAddress, topics, startingBlock, tag){
     }, (error, result) => {
         if (error)
             console.error(error);
-            console.log(result);
+        //console.log(result);
     }).on("connected",(subscriptionId)=> {
-        console.log("Suscribed to "+tag, subscriptionId);
+        //console.log("Suscribed to "+tag, subscriptionId);
     })
-    .on("data",(log) => {
+    .on("data",async (log) => {
         //console.log(new Date().toISOString() +" update on "+tag, log);
         //should be an array that we traverse
-        const finalPayload = obtainPayloadFromLogData(contractAddress, topics, log);
-        console.log(new Date().toISOString() +" update on "+tag, finalPayload[0].rewardData);
+        const finalPayload = await obtainPayloadFromLogData(contractAddress, topics, log);
+        finalPayload.forEach(element => {
+            console.log(new Date().toISOString() +" update on "+ tag);
+            console.log(element.rewardData);
+        });
+        //console.log(">> final data", finalPayload);
         //store on database using tag
     });
 }
 
-function parseMarketplaceTxData(data){
+function parseMarketplaceTxData(data, hash, lIndex){
     let rewards = [];
     const parsedData =web3.eth.abi.decodeParameters(['tuple(address,uint256[],uint256[],address[])[]'], data);   
     const dataElement = parsedData[0][0];
     for (let index = 0; index < dataElement[3].length; index++) {
-        rewards.push({walletAddress: dataElement[3][index], reward: dataElement[2][index]? dataElement[2][index]: 0 });
+        rewards.push({
+            walletAddress: dataElement[3][index], 
+            reward: dataElement[2][index]? dataElement[2][index]: 0 , 
+            eventType: 'purchase',
+            event_id: 'purchase-'+hash+'-'+lIndex+'-'+index
+
+        });
     }
     return rewards;
 }
 
-function parseFactoryTxData(topic, data){
-    return data
+async function parseFactoryTxData(topic, hash, lIndex){
+    try{
+        const collectionAddress = web3.eth.abi.decodeParameters(
+            ['address'], 
+            topic
+        ); 
+        const collection = new web3.eth.Contract(creatorABI, collectionAddress[0]);
+        const walletAddress = await collection.methods.creator().call()
+        return {
+            walletAddress,
+            reward: 0,
+            eventType: 'deployedCollection',
+            event_id: 'deployedCollection-'+hash+'-'+lIndex
+        }
+    } catch (error){
+        console.log(error)
+        return {
+            walletAddress:'0x0000000000000000000000000000000000000000',
+            reward: 0,
+            eventType: 'deployedCollection'
+        }
+    }
 }
 
-function obtainPayloadFromLogData(address, topics, log){
+async function obtainPayloadFromLogData(address, topics, log){
     let payments = [];
     let payload = JSON.parse(JSON.stringify(log));
     let rewardStructure = [];
     switch (address) {
         case contracts['marketplace'].contractAddress:
-            rewardStructure.push(parseMarketplaceTxData(payload.data));
+            rewardStructure.push(parseMarketplaceTxData(payload.data, log.transactionHash, log.logIndex));
             break;
         case contracts['factory'].contractAddress:
-            rewardStructure.push(parseFactoryTxData(topics[0], payload.data));
+            rewardStructure.push(await parseFactoryTxData(log.topics[1], log.transactionHash, log.logIndex));
             break;
         default:
             break;
