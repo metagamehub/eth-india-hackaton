02c4e1b3705a8dad1a9d518698cda75469315298
loladmebrah <shernandezh@unal.edu.co>
2022-10-28 17:42:13 -0500

    indexer updated

--- indexer/index.js
+++ indexer/index.js
@@ -19,17 +19,58 @@ async function subscribeToEvents(contractAddress, topics, startingBlock, tag){
         console.log("Suscribed to "+tag, subscriptionId);
     })
     .on("data",(log) => {
-        console.log(new Date().toISOString() +" update on "+tag, log);
+        //console.log(new Date().toISOString() +" update on "+tag, log);
+        //should be an array that we traverse
+        const finalPayload = obtainPayloadFromLogData(contractAddress, topics, log);
+        console.log(new Date().toISOString() +" update on "+tag, finalPayload[0].rewardData);
         //store on database using tag
     });
 }
 
+function parseMarketplaceTxData(data){
+    let rewards = [];
+    const parsedData =web3.eth.abi.decodeParameters(['tuple(address,uint256[],uint256[],address[])[]'], data);   
+    const dataElement = parsedData[0][0];
+    for (let index = 0; index < dataElement[3].length; index++) {
+        rewards.push({walletAddress: dataElement[3][index], reward: dataElement[2][index]? dataElement[2][index]: 0 });
+    }
+    return rewards;
+}
+
+function parseFactoryTxData(topic, data){
+    return data
+}
+
+function obtainPayloadFromLogData(address, topics, log){
+    let payments = [];
+    let payload = JSON.parse(JSON.stringify(log));
+    let rewardStructure = [];
+    switch (address) {
+        case contracts['marketplace'].contractAddress:
+            rewardStructure.push(parseMarketplaceTxData(payload.data));
+            break;
+        case contracts['factory'].contractAddress:
+            rewardStructure.push(parseFactoryTxData(topics[0], payload.data));
+            break;
+        default:
+            break;
+    }
+
+    rewardStructure.forEach(element => {
+       payments.push({...payload, rewardData: element});
+    });
+
+    return payments;
+}
 
 async function run(){
-    contracts.forEach(async (element) => {
-        const { contractAddress, topics, startingBlock, tag} = element;
-        await subscribeToEvents(contractAddress, topics, startingBlock, tag);
-    });
+    for (const key in contracts) {
+        if (Object.hasOwnProperty.call(contracts, key)) {
+            const element = contracts[key];
+            const { contractAddress, topics, startingBlock, tag} = element;
+            await subscribeToEvents(contractAddress, topics, startingBlock, tag);
+        }
+    }
 }
 
 run();
