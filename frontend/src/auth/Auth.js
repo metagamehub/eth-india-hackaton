39edf921e9836e1d34b0b4482bba1fb27dd0f060
GabCh15 <info@thedac.info>
2022-10-29 13:24:03 -0500

    Router setup

--- frontend/src/auth/Auth.js
+++ frontend/src/auth/Auth.js
@@ -1,26 +1,31 @@
-import React from 'react'
-import { Activity } from '../dashboard/Activity'
-import { Inventory } from '../dashboard/Inventory'
+import React,{useEffect} from 'react'
 import { ConnectionButton } from './ConnectionButton'
+import { useNavigate } from 'react-router-dom'
+import { useAccount } from '@web3modal/react'
 
-export const Login = () => {
-  return (
-    <div className='flex justify-center min-h-screen items-center text-base bg-background text-white'>
-      <div className='text-center'>
-        <h1>
-          METAVERSE
-        </h1>
-        <h2 className='pb-20'>
-          LOYALTY MODULE
-        </h2>
-        <p className='font-body text-white w-96 pb-14'>
-        Social engagement module to incentivize users to participate in the Decentraland ecosystem through different onchain and offchain activities.
-        </p>
-        <ConnectionButton/>
-      </div>
-      <div className='flex flex-col pb-10'>
-      </div>
+export const Auth = () => {
+    const navigate = useNavigate()
+    const { account } = useAccount()
 
-    </div>
-  )
+
+    useEffect(() => {
+        {
+            account.isConnected && navigate('/dashboard',{replace:true})
+        }
+    }, [account])
+    return (
+        <div className="flex justify-center min-h-screen items-center text-base bg-background text-white">
+            <div className="text-center">
+                <h1>METAVERSE</h1>
+                <h2 className="pb-20">LOYALTY MODULE</h2>
+                <p className="font-body text-white w-96 pb-14">
+                    Social engagement module to incentivize users to participate
+                    in the Decentraland ecosystem through different onchain and
+                    offchain activities.
+                </p>
+                <ConnectionButton />
+            </div>
+            <div className="flex flex-col pb-10"></div>
+        </div>
+    )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 import { Web3Modal } from '@web3modal/react'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-    <Web3Modal config={ {
-        projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
-        theme: "dark",
-        accentColor: "default",
-        ethereum: {
-          appName: 'web3Modal',
-          autoConnect: true
-        }
-      }}/>
-  </React.StrictMode>
-);
-
+    <React.StrictMode>
+        <App />
+        <Web3Modal
+            config={{
+                projectId: process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
+                theme: 'dark',
+                accentColor: 'default',
+                ethereum: {
+                    appName: 'web3Modal',
+                    autoConnect: true,
+                },
+            }}
+        />
+    </React.StrictMode>
+)
