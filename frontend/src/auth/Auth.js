cff996bcda6ce9a44639311a208c1dd75b5dad1c
Sebastian Ortiz <jsoc1999@gmail.com>
2022-10-28 22:47:38 -0500

    Minior changes login

--- frontend/src/auth/Auth.js
+++ frontend/src/auth/Auth.js
@@ -3,15 +3,15 @@ import { ConnectionButton } from './ConnectionButton'
 
 export const Login = () => {
   return (
-    <div className='flex justify-center min-h-screen items-center space-y-20 bg-background text-white'>
+    <div className='flex justify-center min-h-screen items-center text-base bg-background text-white'>
       <div className='text-center'>
         <h1>
           METAVERSE
         </h1>
-        <h2>
+        <h2 className='pb-20'>
           LOYALTY MODULE
         </h2>
-        <p className='font-body text-white text-base w-96'>
+        <p className='font-body text-white w-96 pb-14'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
