f8292a3d4fff033cb3a9af1aaaba4a39d0987f10
Sebastian Ortiz <jsoc1999@gmail.com>
2022-10-28 19:01:02 -0500

    Frontend alpha

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      title: ["Anton", "sans-serif"],
+      body: ["Fira Sans", "sans-serif"]
+    },
+    screens: {
+      'xs': '400px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    }
+  },
+  plugins: [],
+}
