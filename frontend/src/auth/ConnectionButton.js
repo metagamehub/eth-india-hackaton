b6f330a8e828943d32d8afde5eb1e0593c9af75a
GabCh15 <info@thedac.info>
2022-10-29 13:39:59 -0500

    Button style recover

--- frontend/src/auth/ConnectionButton.js
+++ frontend/src/auth/ConnectionButton.js
@@ -5,7 +5,7 @@ export const ConnectionButton = () => {
     const { open } = useConnectModal()
     return (
         <>
-            <button
+            <button className="pt-1 z-10 border-solid border-2 w-48 h-12 rounded-xl border-white"
                 onClick={() => {
                     open()
                 }}
