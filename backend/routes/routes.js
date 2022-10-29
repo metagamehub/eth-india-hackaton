d1dbcd61c6a9fca03d54be6db380791d95e8654d
camilourd <camilourd@gmail.com>
2022-10-29 18:02:08 -0500

    Wearables routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,9 +3,11 @@ const routes = express.Router();
 
 const databaseRoutes = require("./databaseRoutes").databaseRoutes;
 const ipfsRoutes = require("./ipfsRoutes.js").ipfsRoutes;
+const wearablesRoutes = require("./wearablesRoutes.js").wearablesRoutes;
 
 routes.use("/", databaseRoutes);
 routes.use("/", ipfsRoutes);
+routes.use("/", wearablesRoutes);
 
 routes.get("/", function (req, res) {
 	res.send("API working !!");
