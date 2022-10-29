e4b0727d8bb7d4d8c62283aa6d1235ab2d1bc7d7
Shinuamai <laurad00sandovalg@gmail.com>
2022-10-29 18:00:59 -0500

    level progress, wearable and modifcations

--- frontend/src/components/wearable.js
+++ frontend/src/components/wearable.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export const Wearable = ({ name, wearable, onClick, clicked }) => {
+export const Wearable = ({ name, url, description ,image_class , onClick, clicked }) => {
     return (
 
         <div 
             onClick={onClick ? () => onClick() : () => { }}
-            className={`flex flex-col bg-[#262626] relative text-center m-1 items-center justify-center space-y-1 sm:space-y-2 w-32 md:w-40 h-32 md:h-40 rounded ${onClick ? "cursor-pointer" : "cursor-default"} hover:scale-105 transition duration-200 ease-linear ${clicked ? "border-opacity-30" : "border-opacity-5"}`}
+            className={`flex flex-col bg-[#262626] relative text-center m-1 items-center justify-center space-y-1 sm:space-y-2 w-30 lg:w-35 xl:w-40 h-30 lg:h-35 xl:h-40 rounded ${onClick ? "cursor-pointer" : "cursor-default"} hover:scale-105 transition duration-200 ease-linear ${clicked ? "border-opacity-30" : "border-opacity-5"}`}
         >
-            <img src={`/images/${name}.png`} className={`group-hover:grayscale-0 transition duration-300 ease-in-out object-contain`} />
+            <img src={url ? url : `/images/${name}.png`} alt={description} className={image_class} />
         </div>
     )
 }
