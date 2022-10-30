93239f98fa29e9cd31cd88a86dd683e63f5c1683
Sebastian Ortiz <jsoc1999@gmail.com>
2022-10-29 23:14:01 -0500

    Button changes

--- frontend/src/dashboard/Activity.js
+++ frontend/src/dashboard/Activity.js
@@ -11,7 +11,7 @@ export const Activity = () => {
     {openModal && <RewardModal onDismiss={() => setOpenModal(false)} />}
     <div className="flex flex-col space-y-3 w-90 text-white">
       <h2 className="text-xl truncate">ACTIVITY 
-      <span onClick={()=> setOpenModal(true)}> Claim all</span>
+        <Button text="Claim all" onClick={()=> setOpenModal(true)} />
       </h2>
       <div className="flex h-10 bg-grey rounded-[25px] space-x-0 justify-between">
           <p className="font-fire text-md py-2 pl-4">Bought weareable</p>
