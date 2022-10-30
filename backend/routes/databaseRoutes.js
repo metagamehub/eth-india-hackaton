2c72dcbe2c335a1a40a07b610e3ee1558128c57b
GabCh15 <info@thedac.info>
2022-10-30 03:14:03 -0500

    Push

--- backend/routes/databaseRoutes.js
+++ backend/routes/databaseRoutes.js
@@ -1,23 +1,27 @@
-const express = require("express");
-const routes = express.Router();
-const controller = require("../controller/controller");
+const express = require('express')
+const routes = express.Router()
+const controller = require('../controller/controller')
 
-routes.get("/db/read", controller.read);
+routes.get('/db/read', controller.read)
 
-routes.get("/db/read-wallet", controller.readAllWalletEvents);
+routes.get('/db/read-wallet', controller.readAllWalletEvents)
 
-routes.get("/db/read-purchases", controller.readPurchases);
+routes.get('/db/read-purchases', controller.readPurchases)
 
-routes.get("/db/read-votes", controller.readVotes);
+routes.get('/db/read-votes', controller.readVotes)
 
-routes.get("/db/read-proposals", controller.readProposal);
+routes.get('/db/read-proposals', controller.readProposal)
 
-routes.get("/db/read-top", controller.readTop);
+routes.get('/db/read-top', controller.readTop)
 
-routes.post("/db/create", controller.create);
+routes.post('/db/create', controller.create)
 
-routes.put("/db/update", controller.update);
+routes.put('/db/update', controller.update)
 
-routes.delete("/db/delete", controller.delete);
+routes.delete('/db/delete', controller.delete)
 
-exports.databaseRoutes = routes;
+routes.post('/db/claimTokens', controller.claimTokens)
+
+routes.post('/db/claimBadges', controller.claimBadges)
+
+exports.databaseRoutes = routes
--- frontend/src/modals/RewardModal.js
+++ frontend/src/modals/RewardModal.js
@@ -1,20 +1,37 @@
+import axios from 'axios'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-export const RewardModal = ({onDismiss}) => {
-  return (
-    <>
-      <div className='modal text-center text-white'>
-      <div onClick={onDismiss} className="absolute h-full w-full bg-black bg-opacity-40 backdrop-filter backdrop-blur" />
-        <div className="z-10 w-96 transform scale-85 sm:scale-100 flex flex-col items-stretch shadow-dark p-5 space-y-7 rounded-xl border border-white border-opacity-20 bg-grey-darkest bg-opacity-20 backdrop-filter backdrop-blur-xl">
-        <h2 className='max-h-[3.3rem] mt-11'>REWARD</h2>
-        <p className='font-body text-white w-96 pb-14 truncate'>
-          asdasdfs
-        </p>
-        <div className='regularButton'>
-          <button className='my-10'>Claim</button>
-        </div>
-      </div>
-    </div>
-    </>
-  )
+export const RewardModal = ({ onDismiss }) => {
+  const wallet = useSelector((state) => state.wallet)
+    return (
+        <>
+            <div className="modal text-center text-white">
+                <div
+                    onClick={onDismiss}
+                    className="absolute h-full w-full bg-black bg-opacity-40 backdrop-filter backdrop-blur"
+                />
+                <div className="z-10 w-96 transform scale-85 sm:scale-100 flex flex-col items-stretch shadow-dark p-5 space-y-7 rounded-xl border border-white border-opacity-20 bg-grey-darkest bg-opacity-20 backdrop-filter backdrop-blur-xl">
+                    <h2 className="max-h-[3.3rem] mt-11">REWARD</h2>
+                    <p className="font-body text-white w-96 pb-14 truncate">
+                        asdasdfs
+                    </p>
+                    <div className="regularButton">
+                        <button
+                            onClick={() => {
+                                axios.post(
+                                    process.env
+                                        .REACT_APP_WALLETCONNECT_BACKEND_URL +
+                                        '/db/claimTokens?walletAddress='+wallet.address,
+                                )
+                            }}
+                            className="my-10"
+                        >
+                            Claim
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
