0166d715544ba1d0ae1f316c3159e9f6bd7f49e8
DAC-info <info@thedac.info>
2022-12-02 18:43:00 -0500

    notification types

--- backend/services/pushNotifications.js
+++ backend/services/pushNotifications.js
@@ -1,46 +1,62 @@
-require("dotenv").config()
+require('dotenv').config()
 
-const PushAPI = require("@pushprotocol/restapi")
-const ethers = require("ethers")
+const PushAPI = require('@pushprotocol/restapi')
+const ethers = require('ethers')
 
+const signer = new ethers.Wallet(process.env.PUSH_DELEGATE_KEY)
 
-const signer = new ethers.Wallet(process.env.PUSH_DELEGATE_KEY);
+const getNotificationInput = (eventType) => {}
 
-const getNotificationInput = (eventType) => {
-
+const notificationTypes = {
+    point: {
+        notification: { title: '', body: '' },
+        payload: { title: '', body: '' },
+    },
+    badge: {
+        notification: { title: '', body: '' },
+        payload: { title: '', body: '' },
+    },
+    event: {
+        notification: { title: '', body: '' },
+        payload: { title: '', body: '' },
+    },
 }
 
-
-const sendNotification = async(
+const sendNotification = async (
+    notificationType,
     recipient,
-    
+    imageUrl,
+    titleSuffix,
+    bodySuffix,
+    expiryMinutesLeft
 ) => {
-  try {
-    const apiResponse = await PushAPI.payloads.sendNotification({
-      signer,
-      type: 3, // target
-      identityType: 2, // direct payload
-      notification: {
-        title: `[SDK-TEST] notification TITLE:`,
-        body: `[sdk-test] notification BODY`
-      },
-      payload: {
-        title: `[sdk-test] payload title`,
-        body: `sample msg body`,
-        cta: '',
-        img: ''
-      },
-      expiry: 0,
-      recipients: 'eip155:1:0xa9aa5CA3acE7252121D35711db0d7b8ECAca6B1E', // recipient address
-      channel: 'eip155:1:0xa5090e5819BBA73d1921d6678B199E6949e7ac3a', // your channel address
-      env: 'prod'
-    });
-    
-    // apiResponse?.status === 204, if sent successfully!
-    console.log('API repsonse: ', apiResponse);
-  } catch (err) {
-    console.error('Error: ', err);
-  }
+    let { notification, payload } = notificationTypes[notificationType]
+    payload.img = imageUrl
+    payload.title += titleSuffix
+    payload.body += bodySuffix
+    payload.cta =process.env.MLM_URL
+    let expiry
+    if(expiryMinutesLeft)
+     expiry = Date.now() / 1000 + expiryMinutesLeft * 60
+    const type = recipient ? 3 : 1
+    try {
+        const apiResponse = await PushAPI.payloads.sendNotification({
+            signer,
+            type, // target
+            identityType: 2, // direct payload
+            notification,
+            payload,
+            expiry,
+            recipients: `eip155:1:${recipient}`, // recipient address
+            channel: `eip155:1:${process.env.PUSH_CHANNEL_ADDRESS}`, // your channel address
+            env: 'prod',
+        })
+
+        // apiResponse?.status === 204, if sent successfully!
+        console.log('API repsonse: ', apiResponse)
+    } catch (err) {
+        console.error('Error: ', err)
+    }
 }
 
-sendNotification();
+sendNotification()
